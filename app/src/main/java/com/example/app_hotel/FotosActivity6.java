819ce9206ddfbd4c0f03f7dd0ec1aa6d16c1e856
Helmut Josue Colindres Brenes <helmut-1521@hotmail.com>
2020-06-23 05:44:40 +0100

    agregando mas cambios a la activity de Galeria

--- app/src/main/java/com/example/app_hotel/FotosActivity6.java
+++ app/src/main/java/com/example/app_hotel/FotosActivity6.java
@@ -3,12 +3,19 @@ package com.example.app_hotel;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
 
 public class FotosActivity6 extends AppCompatActivity {
 
+    private ImageView _foto;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fotos6);
+
+        _foto=findViewById(R.id.foto);
+
+        String foto = "galeria" + getIntent().getExtras().getString("galeria");
+        _foto.setImageResource(getResources().getIdentifier(foto,"drawable",getPackageName()));
     }
 }
--- app/src/main/java/com/example/app_hotel/GaleriaActivity5.java
+++ app/src/main/java/com/example/app_hotel/GaleriaActivity5.java
@@ -2,13 +2,44 @@ package com.example.app_hotel;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
-public class GaleriaActivity5 extends AppCompatActivity {
+public class GaleriaActivity5 extends AppCompatActivity implements View.OnClickListener {
 
+    private ImageView f1,f2,f3,f4,f5,f6,f7,f8,f9,f10;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_galeria5);
+
+        f1=findViewById(R.id.foto1);
+        f2=findViewById(R.id.foto2);
+        f3=findViewById(R.id.foto3);
+        f4=findViewById(R.id.foto4);
+        f5=findViewById(R.id.foto5);
+        f6=findViewById(R.id.foto6);
+        f7=findViewById(R.id.foto7);
+        f8=findViewById(R.id.foto8);
+        f9=findViewById(R.id.foto9);
+        f10=findViewById(R.id.foto10);
+
+        f1.setOnClickListener(this);
+        f2.setOnClickListener(this);
+        f3.setOnClickListener(this);
+        f4.setOnClickListener(this);
+        f5.setOnClickListener(this);
+        f6.setOnClickListener(this);
+        f7.setOnClickListener(this);
+        f8.setOnClickListener(this);
+        f9.setOnClickListener(this);
+        f10.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        startActivity(new Intent(getApplicationContext(),FotosActivity6.class).putExtra("galeria",view.getId()));
     }
 }
